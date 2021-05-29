3df6b7576974b46fedbedf0489d417b377cafd11
Rostand Nj <macbookpro@MBP-rostandnj.local>
2021-05-29 11:10:48 +0100

    inkspace

---
+++ pages/posts/first-post.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Path, {Svg}  from 'react-svg-path';
+import Roadmap from '../Component/RoadMap'
+
+
+export default function FirstPost() {
+    const width = 800;
+    const height = 800;
+    return (
+        <Roadmap />
+
+    );
+}
+
+const click = () => {
+    console.log('111');
+}
